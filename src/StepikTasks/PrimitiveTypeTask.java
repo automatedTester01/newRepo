aae85016d6b8e4c96d404c82459271346a2403cb
automatedTester01 <konstantin642@mail.ru>
2019-12-05 01:05:34 +0300

    first commit

---
+++ src/StepikTasks/PrimitiveTypeTask.java
@@ -0,0 +1,20 @@
+package StepikTasks;
+
+/* Реализуйте метод, возвращающий true, если среди четырех его аргументов ровно два истинны (любые). Во всех остальных случаях метод должен возвращать false.
+Воспользуйтесь шаблоном кода, который предлагает система. Ввод-вывод будет сделан за вас. Вам надо только проанализировать переданные в метод
+booleanExpression значения (a, b, c, d) и вернуть результат. Попробуйте составить формулу с использованием булевых операторов.
+Если не получается, вернитесь к этому заданию после просмотра степов про условные операторы и циклы.
+*/
+
+public class PrimitiveTypeTask {
+    public static void main(String args[]) {
+       if (booleanExpression (true, true, false, false) == true)
+           System.out.println("Результат: true");
+       else
+           System.out.println("Результат: false");
+    }
+
+    public static boolean booleanExpression(boolean a, boolean b, boolean c, boolean d) {
+        return (a & b & !c & !d) || (a & !b & c & !d) || (!a & !b & c & d) || (!a & b & !c & d) || (!a & b & c & !d) || (a & !b & !c & d);
+    }
+}
